f0b3c9ca5a9a5c34b07d372528c412e41fd05085
Yujung Lee <yujunglee@yujungui-macbook-pro.local>
2016-06-20 22:39:08 +0900

    Initial Commit

---
+++ GestureAvatar10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureAvatar10
+//
+//  Created by Yujung Lee on 2016. 6. 20..
+//  Copyright © 2016년 YujungLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
