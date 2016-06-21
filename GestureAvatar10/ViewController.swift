562d34878abf0058598f2c1e7d7dba2dd1588651
Yujung Lee <yujunglee@yujungui-macbook-pro.local>
2016-06-21 13:34:35 +0900

    A single Video play

--- GestureAvatar10/ViewController.swift
+++ GestureAvatar10/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    let avPlayerViewController = AVPlayerViewController();
+    var avPlayer: AVPlayer?
+    var avplayerlayer: AVPlayerLayer = AVPlayerLayer()
+    var inputText: String = String()
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var videoView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //let urlPathString:String? = NSBundle.mainBundle().pathForResource("Schedule_Demo_1", ofType: "mp4" )//local video
+        
+       // let serverString: String = "http://cspc.sogang.ac.kr/~yjlee127/capstone/"
+       // let movieString: String = serverString + inputText + ".mp4"
+       // let movieUrl: NSURL? = NSURL(string: movieString)
+        let movieUrl: NSURL? = NSURL(string: "http://cspc.sogang.ac.kr/~yjlee127/capstone/Schedule_Demo_1.mp4")
+        
+        //optional binding. movieUrl이 실제로 있는 url인지 확인하기 위함
+        if let url = movieUrl {
+            self.avPlayer = AVPlayer(URL: url)//AVPlayer object를 만듦
+            avplayerlayer = AVPlayerLayer(player: self.avPlayer)
+            avplayerlayer.frame = videoView.bounds
+
+            avplayerlayer.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width*0.85, height: self.view.frame.size.width*0.5)
+            
+            avplayerlayer.videoGravity = AVLayerVideoGravityResizeAspect
+            avplayerlayer.needsDisplayOnBoundsChange = true
+            
+            videoView.layer.addSublayer(avplayerlayer)
+            videoView.layer.needsDisplayOnBoundsChange = true
+            
+            self.avPlayerViewController.player = self.avPlayer
+        }
+ 
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func playButtonTapped(sender: AnyObject) {
+        /*
+        self.presentViewController(self.avPlayerViewController, animated: true) { 
+            ()->Void in self.avPlayerViewController.player?.play()
+        }//재생이 끝나면 반복재생*/
+        self.avPlayer?.play()
+    }
 
+    
+    @IBAction func pauseButtonTapped(sender: AnyObject) {
+        self.avPlayer?.pause()
+    }
+    
+    /* Textfield and Keyboard */
+    //keyboard resign function
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        guard let text: String = textField.text else  {
+            return
+        }
+        inputText = text
+        
+    }
+
+    /* movie play function */
+    func playMovie() {
+        
+    }
 }
 
